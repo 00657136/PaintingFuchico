47a3ab106ffdaf58a4f280caa7e97958d2013e7d
c8610290927 <c8610290927@gmail.com>
2020-03-18 14:22:55 +0800

    Initial Commit

---
+++ Painting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Painting
+//
+//  Created by User20 on 2020/3/18.
+//  Copyright © 2020 00657136. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
